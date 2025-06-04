6a7ba92155dccb6d752b4bca2024f9b5b83edc44
Lorenzo Valentina <valentinalorenzo228@gmail.com>
2025-06-04 18:06:04 +0000

    fix: github pages adapt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     devToolbar: { enabled: false},
+    site: 'https://celestecourtade.github.io',
 });
